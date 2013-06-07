d314533441b0308cd3c56bbe8dc1f66fddd8495b
mactive <mactive@gmail.com>
2013-06-07 17:50:18 +0800

    change api

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'stockbro';
+$db['default']['password'] = 'TG7a6m5hPj7TRLJ8';
+$db['default']['database'] = 'stockbro';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- application/controllers/stock.php
+++ application/controllers/stock.php
@@ -19,12 +19,42 @@ class Stock extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('welcome_message'); 
 	}
 
-	public function realtime()
+	public function realtime($symbol)
 	{
 		echo '{"c": "SH601000","t": 1370575770,"n": "江苏吴中","lc": 3.10,"o": 3.11,"h": 3.14,"l": 3.08,"np": 3.10,"v": 12763,"a": 3961752,"bp": [3.09,3.08,3.07,3.06,3.05], "bv": [311,1082,678,1316,956],"sp": [3.10,3.11,3.12,3.13,3.14],"sv": [164,1152,848,1356,733]}';
+	}
+
+	public function getStockList()
+	{
+		//
+	}
+
+	public function saveStockList()
+	{
+
+		$url = 'http://www.stockbro.com/js/stock_by_line.js';
+		$handle = fopen($url,'rb');
+		$string = stream_get_contents($handle);
+
+		$array = explode("\n", $string);
+		// print_r($array);
+
+		foreach ($array as $key => $value) {
+			# code...
+			$vals = explode(',',$value);
+			$data = array(
+			   'symbol' => $vals[0] ,
+			   'short' => $vals[1] ,
+			   'name' => $vals[2]
+			);
+			print_r($data);
+
+			$this->db->insert('stock_list', $data); 
+		}
+
 	}
 }
 
