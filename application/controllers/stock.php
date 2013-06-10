5211bb7ff3bd06fde66cfa562d61eb31c6660af1
mactive <mactive@gmail.com>
2013-06-10 10:28:45 +0800

    change sql

--- application/controllers/stock.php
+++ application/controllers/stock.php
@@ -4,19 +4,18 @@ class Stock extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 * Stock 
 	 */
+
+	public function __construct()
+	{
+	  	parent::__construct();
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Expose-Headers: Access-Control-Allow-Origin');
+		header('Content-type: application/json');
+	}
+
+
 	public function index()
 	{
 		$this->load->view('welcome_message'); 
@@ -25,11 +24,6 @@ class Stock extends CI_Controller {
 	public function realtime($symbol)
 	{
 		echo '{"c": "SH601000","t": 1370575770,"n": "江苏吴中","lc": 3.10,"o": 3.11,"h": 3.14,"l": 3.08,"np": 3.10,"v": 12763,"a": 3961752,"bp": [3.09,3.08,3.07,3.06,3.05], "bv": [311,1082,678,1316,956],"sp": [3.10,3.11,3.12,3.13,3.14],"sv": [164,1152,848,1356,733]}';
-	}
-
-	public function getStockList()
-	{
-		//
 	}
 
 	public function saveStockList()
